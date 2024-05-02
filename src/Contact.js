16bcd11e79fb35095f893b8f14cc10a9556b7874
nicolecheng7603 <156100927+nicolecheng7603@users.noreply.github.com>
2024-05-01 18:47:55 -0700

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './App.css';
 
 function Contact() {
   return (
-    <div >
-        <h1>Contact</h1>
+    <div>
+      <h2>Contact Page</h2>
+      <p>Information about how to contact you goes here.</p>
     </div>
   );
 }
 
-export default Contact; 
+export default Contact;
